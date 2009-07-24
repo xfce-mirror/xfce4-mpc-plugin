0e761b9adcaddbf4b7e2e3061e57fc8138dfbe2b
Landry Breuil <landry@openbsd.org>
2009-07-24 17:06:19 +0000

    More autohell-fu to correctly install all languages. Do the sed-fu for @REVISION@ and @LINGUAS@ as done in other projects.
    (Old svn revision: 7813)

--- autogen.sh
+++ autogen.sh
@@ -18,6 +18,26 @@ EOF
   exit 1
 }
 
+# verify that po/LINGUAS is present
+(test -f po/LINGUAS) >/dev/null 2>&1 || {
+  cat >&2 <<EOF
+autogen.sh: The file po/LINGUAS could not be found. Please check your snapshot
+            or try to checkout again.
+EOF
+  exit 1
+}
+
+# substitute revision and linguas
+linguas=`sed -e '/^#/d' po/LINGUAS`
+if test -d .git/svn; then
+ revision=`LC_ALL=C git-svn find-rev HEAD`
+else
+ revision=`LC_ALL=C svn info $0 | awk '/^Revision: / {printf "%05d\n",$2}'`
+fi
+sed -e "s/@LINGUAS@/${linguas}/g" \
+    -e "s/@REVISION@/${revision}/g" \
+    < "configure.ac.in" > "configure.ac"
+
 exec xdt-autogen $@
 
 # vi:set ts=2 sw=2 et ai:
