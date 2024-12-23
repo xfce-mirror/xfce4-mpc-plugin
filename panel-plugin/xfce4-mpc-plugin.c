ae493a985535973b4c3a2efb5e0525f0b835bfbb
Gaël Bonithon <gael@xfce.org>
2024-12-23 15:02:40 +0100

    build: Bump dependency versions and fix deprecation warnings

--- panel-plugin/xfce4-mpc-plugin.c
+++ panel-plugin/xfce4-mpc-plugin.c
@@ -270,12 +270,11 @@ mpc_create_options (XfcePanelPlugin * plugin, t_mpc* mpc)
 
    xfce_panel_plugin_block_menu (plugin);
 
-   dlg = xfce_titled_dialog_new_with_buttons (_("Mpd Client Plugin"),
-                                              GTK_WINDOW(gtk_widget_get_toplevel(GTK_WIDGET(plugin))),
-                                              GTK_DIALOG_DESTROY_WITH_PARENT,
-                                              "gtk-close",
-                                              GTK_RESPONSE_OK,
-                                              NULL);
+   dlg = xfce_titled_dialog_new_with_mixed_buttons (_("Mpd Client Plugin"),
+                                                    GTK_WINDOW(gtk_widget_get_toplevel(GTK_WIDGET(plugin))),
+                                                    GTK_DIALOG_DESTROY_WITH_PARENT,
+                                                    "window-close-symbolic", _("_Close"), GTK_RESPONSE_OK,
+                                                    NULL);
    xfce_titled_dialog_set_subtitle (XFCE_TITLED_DIALOG (dlg), _("Properties"));
 
    gtk_window_set_position   (GTK_WINDOW (dlg), GTK_WIN_POS_CENTER);
